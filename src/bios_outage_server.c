748a6fc37599ec97bbf0519105a085ddf934507a
Barbora Stepankova <BarboraStepankova@Eaton.com>
2016-08-15 11:09:02 +0200

    Problem: actor commands are not implemented
    Solution: handling of actor commands added

--- src/bios_outage_server.c
+++ src/bios_outage_server.c
@@ -36,8 +36,8 @@ bios_outage_server (zsock_t *pipe, void *args)
     mlm_client_t *client = mlm_client_new ();
     assert (client);
 
-    int rv = mlm_client_connect (client, "ipc://malamute-test", 1000, "outage");
-    assert (rv >= 0);
+    //    int rv = mlm_client_connect (client, "ipc://malamute-test", 1000, "outage");
+    // assert (rv >= 0);
 
     zpoller_t *poller = zpoller_new (pipe, mlm_client_msgpipe (client), NULL);
     assert(poller);
@@ -59,16 +59,15 @@ bios_outage_server (zsock_t *pipe, void *args)
             else {
                 zsys_debug ("Poller expired");
                 printf("I am alive\n");
+                timestamp = zclock_mono();            
                 continue;
                 }
-            timestamp = zclock_mono();            
         }
         
         int64_t now = zclock_mono();
         
         if (now - timestamp  >= timeout){
-            printf(" >>> I am alive. <<<\n");
-            
+            printf(" >>> I am alive. <<<\n"); 
             timestamp = zclock_mono ();
         }
         
@@ -79,7 +78,7 @@ bios_outage_server (zsock_t *pipe, void *args)
             char *command = zmsg_popstr(msg);
             if (!command) {
                 zmsg_destroy (&msg);
-                zsys_debug ("Prazdny prikaz.");
+                zsys_debug ("Empty command.");
                 continue;
             }
             
@@ -88,19 +87,50 @@ bios_outage_server (zsock_t *pipe, void *args)
                 zstr_free (&command);
                 break;
             }
-            else if (streq(command, "PRINT")) {
+            else if (streq(command, "ENDPOINT")) {
+		char *endpoint = zmsg_popstr (msg);
+		char *name = zmsg_popstr (msg);
+		if (endpoint && name) {
+			int rv = mlm_client_connect (client, endpoint, 1000, name);
+			if (rv >= 0) {
+				printf ("mlm_client_connest OK\n");
+			}
+		}
+		else {
+			printf ("BAD ENDPOINT MESSAGE\n");
+
+		}
+		zstr_free (&endpoint);
+		zstr_free (&name);
+            }    
+            else if (streq (command, "CONSUMER")) {
+                char *stream = zmsg_popstr(msg);
+                assert(stream);
+                char *regex = zmsg_popstr(msg);
+                assert(regex);
+                
+                zstr_free (&stream);
+                zstr_free (&regex);                                
+                zstr_free (&command);
+                zmsg_destroy (&msg);
+                continue;
+            }
+            else if (streq (command, "PRODUCER")){
+                char *stream = zmsg_popstr(msg);
+                assert(stream);
+                
                 zstr_free (&command);
                 zmsg_print (msg);
                 zmsg_destroy (&msg);
                 continue;
-                
-            } else {
-                zsys_debug ("Neznamy prikaz: %s\n", command);
+                 
+            }
+	    else {
+                zsys_debug ("Unknown command: %s. Terminating.\n", command);
+	    }
                 zstr_free (&command);
                 zmsg_destroy (&msg);
                 continue;
-                                
-            }
         }
         assert (which == mlm_client_msgpipe (client));
 
@@ -144,16 +174,40 @@ bios_outage_server_test (bool verbose)
     printf (" * bios_outage_server: ");
 
     //  @selftest
-    
+    static const char *endpoint =  "ipc://malamute-test2";
     zactor_t *server = zactor_new (mlm_server, (void*) "Malamute");
-    zstr_sendx (server, "BIND","ipc://malamute-test", NULL);
+    zstr_sendx (server, "BIND",endpoint, NULL);
     zclock_sleep (100);
-    
+
     
     zactor_t *outsvr = zactor_new (bios_outage_server, (void*) NULL);
     assert (outsvr);
+
+    zstr_sendx (outsvr, "ENDPOINT", endpoint, NULL);
+    zstr_sendx (outsvr, "ENDPOINT",  NULL);
+    zstr_sendx (outsvr, "KARCI", endpoint, "outsvr", NULL);
+    zstr_sendx (outsvr, "ENDPOINT", endpoint, "outsvr", NULL);
     zclock_sleep (100);
 
+    mlm_client_t *sender = mlm_client_new();
+    int rv = mlm_client_connect (sender, endpoint, 100, "sender");
+    assert (rv >= 0);
+
+    zmsg_t *msg = zmsg_new ();
+    zmsg_addstr (msg, "hello");
+
+    printf("\ncekani na zpravu1\n");
+    rv = mlm_client_sendto (sender,"outsvr", "subject", NULL, 1000, &msg);
+    assert (rv >= 0);
+    zclock_sleep (1000);
+    
+    zmsg_t *recv = mlm_client_recv (sender);
+    assert (recv);
+
+    char *recvmsg = zmsg_popstr(recv); 
+    assert (streq(recvmsg,"world"));
+    
+    /*
     mlm_client_t *sender = mlm_client_new();
     int rv = mlm_client_connect (sender,"ipc://malamute-test", 100, "sender");
     assert (rv >= 0);
@@ -170,25 +224,17 @@ bios_outage_server_test (bool verbose)
 
     char *recvmsg = zmsg_popstr(recv); 
     assert (streq(recvmsg,"world"));
-
-    zclock_sleep(1500);
-    zstr_sendx(outsvr,"PRINT","Karol", "Bara", NULL);
-    zclock_sleep(1000);
-    zstr_sendx(outsvr,"PRINT","Karol2", "Bara2", NULL);
-    zclock_sleep(1000);
-    zstr_sendx(outsvr,"PRINT","Karol3", "Bara3", NULL);
-    zclock_sleep(1000);
-    zstr_sendx(outsvr,"PRINT","Karol4", "Bara4", NULL);
-    zclock_sleep(6000);
-    
+    */
+           
     {
     zmsg_t *msg = zmsg_new ();
     zmsg_addstr (msg, "hello");
 
-    rv = mlm_client_sendto (sender,"outage", "subject", NULL, 1000, &msg);
+    rv = mlm_client_sendto (sender,"outsvr", "subject", NULL, 1000, &msg);
     assert (rv >= 0);
     zclock_sleep (100);
 
+    printf("\ncekame na zpravu2\n");
     zmsg_t *recv = mlm_client_recv (sender);
     assert (recv);
 
@@ -205,8 +251,6 @@ bios_outage_server_test (bool verbose)
     zactor_destroy(&outsvr);
     zactor_destroy (&server);
 
-    printf ("koncime\n");
-    
     //  @end
     printf ("OK\n");
 }
