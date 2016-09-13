87bc48f86c83eced493df96458256b8ae1788548
Michal Vyskocil <michalvyskocil@eaton.com>
2016-09-13 08:45:54 +0200

    Problem: previous state should be loaded before messages are processed
    Solution: call STATE-FILE as a first thing
    
    Signed-off-by: Michal Vyskocil <michalvyskocil@eaton.com>

--- src/bios_agent_outage.c
+++ src/bios_agent_outage.c
@@ -60,6 +60,7 @@ int main (int argc, char *argv [])
     zactor_t *self = zactor_new (bios_outage_server, NULL);
     if (verbose)
         zstr_sendx (self, "VERBOSE", NULL);
+    zstr_sendx (self, "STATE-FILE", "/var/lib/bios/agent-outage/state.zpl", NULL);
     zstr_sendx (self, "CONNECT", "ipc://@/malamute", "agent-outage", NULL);
     zstr_sendx (self, "CONSUMER", BIOS_PROTO_STREAM_METRICS, ".*", NULL);
     zstr_sendx (self, "CONSUMER", BIOS_PROTO_STREAM_METRICS_UNAVAILABLE, ".*", NULL);
@@ -67,7 +68,6 @@ int main (int argc, char *argv [])
     zstr_sendx (self, "CONSUMER", BIOS_PROTO_STREAM_ASSETS, ".*", NULL);
     zstr_sendx (self, "PRODUCER", BIOS_PROTO_STREAM_ALERTS_SYS, NULL);
     zstr_sendx (self, "TIMEOUT", "30000", NULL);
-    zstr_sendx (self, "STATE-FILE", "/var/lib/bios/agent-outage/state.zpl", NULL);
 
     // src/malamute.c, under MPL license
     while (true) {
