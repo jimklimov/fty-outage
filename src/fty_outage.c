f17c375e1a4c472211e9f9088f721a892e92fbab
Michal Vyskocil <michalvyskocil@eaton.com>
2017-01-05 08:44:48 +0100

    Problem: fty-outage do not start server
    Solution: start it
    
    Signed-off-by: Michal Vyskocil <MichalVyskocil@eaton.com>

--- src/fty_outage.c
+++ src/fty_outage.c
@@ -55,5 +55,19 @@ int main (int argc, char *argv [])
 
     if (verbose)
         zsys_info ("fty_agent_outage - Agent outage");
+
+    zactor_t *server = zactor_new (fty_outage_server, "outage");
+    while (true) {
+        char *str = zstr_recv (server);
+        if (str) {
+            puts (str);
+            zstr_free (&str);
+        }
+        else {
+            puts ("Interrupted ...");
+            break;
+        }
+    }
+    zactor_destroy (&server);
     return 0;
 }
