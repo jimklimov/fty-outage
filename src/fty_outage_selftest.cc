0c262b62b74dc166fc4152d7e655746c4e820ad1
Jim Klimov <jim@jimklimov.com>
2019-10-10 11:01:43 +0200

    Regenerate for fty-common now requiring cxxtools

--- src/fty_outage_selftest.cc
+++ src/fty_outage_selftest.cc
@@ -40,14 +40,14 @@ typedef struct {
 static test_item_t
 all_tests [] = {
 #ifdef FTY_OUTAGE_BUILD_DRAFT_API
-// Tests for draft public classes:
-    { "fty_outage_server", fty_outage_server_test, false, true, NULL },
-#endif // FTY_OUTAGE_BUILD_DRAFT_API
-#ifdef FTY_OUTAGE_BUILD_DRAFT_API
 // Tests for stable/draft private classes:
 // Now built only with --enable-drafts, so even stable builds are hidden behind the flag
     { "data", NULL, true, false, "data_test" },
     { "private_classes", NULL, false, false, "$ALL" }, // compat option for older projects
+#endif // FTY_OUTAGE_BUILD_DRAFT_API
+#ifdef FTY_OUTAGE_BUILD_DRAFT_API
+// Tests for draft public classes:
+    { "fty_outage_server", fty_outage_server_test, false, true, NULL },
 #endif // FTY_OUTAGE_BUILD_DRAFT_API
     {NULL, NULL, 0, 0, NULL}          //  Sentinel
 };
